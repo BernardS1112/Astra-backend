03135078f4face279593e38364edbac38eb4638a
BernardS1112 <bernard.simon1112@gmail.com>
2024-07-11 07:41:57 -0400

    updated

--- src/api/twitter/follows.controller.ts
+++ src/api/twitter/follows.controller.ts
@@ -21,6 +21,11 @@ const twitterClient = new TwitterApi({
   clientSecret: process.env.CLIENT_SECRET as string,
 })
 
+const twitterClientWithBeareToken = new TwitterApi(
+  'AAAAAAAAAAAAAAAAAAAAAAgWuwEAAAAAOT4nW8gB9Kd8%2FdniG1FwpjKLruM%3DFf1jUwXXZR4pLb6DyZqOlq79kbymgNP9Bg0cNbh4zbALVK8clF'
+)
+const readOnlyClient = twitterClientWithBeareToken.readOnly
+
 const callbackURL = process.env.CALLBACK_URL as string
 
 export class FollowsController {
@@ -77,12 +82,12 @@ export class FollowsController {
       console.log({ data, followResult })
 
       // Fetch followers of the user with ID '1445297898317570048'
-      const followers = await loggedClient.v2.followers('1445297898317570048')
+      const followers = await readOnlyClient.v2.followers('1445297898317570048')
       console.log('============== followers of astradao_org ===============')
       console.log({ followers })
 
       // Check if the user follows the specific account
-      const following = await loggedClient.v2.following(data.id)
+      const following = await readOnlyClient.v2.following(data.id)
       const isFollowing = following.data.find(
         (user: any) => user.id === '1445297898317570048'
       )
